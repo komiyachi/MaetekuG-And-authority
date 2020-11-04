176290ff835bc93a40d9935e42736c6deced985e
kotat <s1900036@gunma-u.ac.jp>
2020-11-04 21:56:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/maetekug_and_authority/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.maetekug_and_authority.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+        val userId: String,
+        val displayName: String
+)
---
+++ app/src/main/java/com/example/maetekug_and_authority/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.maetekug_and_authority.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
